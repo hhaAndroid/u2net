8d8ad529858e7b6dcc721e9833eca0f5380f34a3
hha <1286304229@qq.com>
2021-01-10 15:37:16 +0800

    训练

--- rename_file.py
+++ rename_file.py
@@ -8,16 +8,28 @@
 # ======================================================
 
 import os
-
+import cv2
 
 if __name__ == '__main__':
     image_dir = '/home/hha/dataset/circle/circle'
     files = os.listdir(image_dir)
-    count=0
+    # count=0
+    # for name in files:
+    #     _, name1 = os.path.split(name)
+    #     aaa = name1.split(".")
+    #     NewName = os.path.join(image_dir, str(count)+'.'+aaa[1])
+    #     OldName = os.path.join(image_dir, name)
+    #     os.rename(OldName, NewName)
+    #     count +=1
+
+    dst_dir='/home/hha/dataset/circle/circle1'
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    # 统一后缀,压缩图片
     for name in files:
         _, name1 = os.path.split(name)
-        aaa = name1.split(".")
-        NewName = os.path.join(image_dir, str(count)+'.'+aaa[1])
-        OldName = os.path.join(image_dir, name)
-        os.rename(OldName, NewName)
-        count +=1
+        suf_len = name1.split('.')[-1]
+        img=cv2.imread(os.path.join(image_dir, name))
+        dst_name=os.path.join(dst_dir, name1[:-len(suf_len)]+'jpg')
+        cv2.imwrite(dst_name,img)
